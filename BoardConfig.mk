074ede0e4f757e315aa3a4fa72a93cd1c63ded63
PlayfulGod <playfulgod@gmail.com>
2014-07-12 12:18:48 -0400

    zee: add proper kernel cmdline and bypasses.
    Change-Id: Ie6740429493e58d3ac9925bbc2d2394b4e1bae2b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,18 +16,15 @@
 
 include device/lge/g2-common/BoardConfigCommon.mk
 
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=z user_debug=31 msm_rtb.filter=0x0 mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_lgd_poled_720p_video
+
 ### Temporary until proper changes are added to kernel/lge/msm8974
-BOARD_MKBOOTIMG_ARGS  := --ramdisk_offset 0x05000000 --dt device/lge/zee/dt.img --tags_offset 0x04800000 ## For prebuilt kernel use to bypass g2-common
+BOARD_MKBOOTIMG_ARGS  := --ramdisk_offset 0x05000000 --dt device/lge/zee/dt.img --tags_offset 0x04800000
 TARGET_PREBUILT_KERNEL := device/lge/zee/kernel ## For initial recovery builds
 BOARD_KERNEL_SEPARATED_DT := ## For prebuilt kernel use to bypass g2-common
 BOARD_CUSTOM_BOOTIMG_MK := ## For prebuilt kernel use to bypass g2-common
-#TARGET_KERNEL_CONFIG := z-att-perf_defconfig
 TARGET_KERNEL_SOURCE := #kernel/lge/zee
 ### End Temporary ###
-
-#TARGET_KERNEL_CONFIG := cyanogenmod_d802_defconfig
-
-#G2_DTS_TARGET := msm8974-z-att
 
 ## Recovery
 BOARD_RECOVERY_SWIPE := true
